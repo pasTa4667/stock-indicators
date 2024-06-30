79a566311b1ea03aa764de33896116bd51cb3a0b
pasTa <langbeinfelix8@gmail.com>
2024-06-30 10:34:44 +0200

    updated utils with generics

--- src/main/java/org/indic/indicators/IndicatorUtils.java
+++ src/main/java/org/indic/indicators/IndicatorUtils.java
@@ -18,10 +18,10 @@ public class IndicatorUtils {
      * @param period
      * @return the sma as OptionalDouble
      */
-    public static OptionalDouble smaWithPeriodValues(List<Double> prices, int period) {
+    public static <T extends Number> OptionalDouble smaWithPeriodValues(List<T> prices, int period) {
         if(prices.size() < period) return OptionalDouble.empty();
         return IntStream.range(prices.size() - period, prices.size())
-                .mapToDouble(prices::get)
+                .mapToDouble((i) -> prices.get(i).doubleValue())
                 .average();
     }
 
@@ -34,11 +34,11 @@ public class IndicatorUtils {
      * @param period
      * @return The sma as OptionalDouble
      */
-    public static OptionalDouble sma(List<Double> prices, int period) {
+    public static <T extends Number> OptionalDouble sma(List<T> prices, int period) {
         if(prices.size() < period) return OptionalDouble.empty();
         return IntStream.rangeClosed(0, prices.size() - period)
                 .mapToDouble(i -> prices.subList(i, i + period).stream()
-                        .mapToDouble(Double::doubleValue).average().orElse(0.0))
+                        .mapToDouble(Number::doubleValue).average().orElse(0.0))
                 .average();
     }
 
@@ -48,10 +48,10 @@ public class IndicatorUtils {
      * @param period
      * @return The sma values as list
      */
-    public static List<Double> smaList(List<Double> prices, int period) {
+    public static <T extends Number> List<Double> smaList(List<T> prices, int period) {
         if (prices.size() < period) return Collections.emptyList();
         return IntStream.rangeClosed(0, prices.size() - period)
-                .mapToDouble(i -> prices.subList(i, i + period).stream().mapToDouble(Double::doubleValue).average().orElse(0.0))
+                .mapToDouble(i -> prices.subList(i, i + period).stream().mapToDouble(Number::doubleValue).average().orElse(0.0))
                 .boxed()
                 .toList();
     }
@@ -63,7 +63,7 @@ public class IndicatorUtils {
      * @param period
      * @return The ema values as list
      */
-    public static List<Double> ema(List<Double> prices, int period) {
+    public static <T extends Number> List<Double> ema(List<T> prices, int period) {
         if(prices.size() < period) return new ArrayList<>();
 
         List<Double> emaValues = new ArrayList<>();
@@ -72,7 +72,7 @@ public class IndicatorUtils {
         double multiplier = 2.0 / (period + 1);
 
         for(int i = period; i < prices.size(); i++) {
-            ema = (prices.get(i) - ema) * multiplier + ema;
+            ema = (prices.get(i).doubleValue() - ema) * multiplier + ema;
             emaValues.add(ema);
         }
 
@@ -86,16 +86,16 @@ public class IndicatorUtils {
      * @param period
      * @return The ema values as list
      */
-    public static List<Double> emaWithFirstPriceAsInitial(List<Double> prices, int period) {
+    public static <T extends Number> List<Double> emaWithFirstPriceAsInitial(List<T> prices, int period) {
         if(prices.size() < period) return new ArrayList<>();
 
         List<Double> emaValues = new ArrayList<>();
-        double ema = prices.get(0);
+        double ema = prices.get(0).doubleValue();
         emaValues.add(ema);
         double multiplier = 2.0 / (period + 1);
 
         for(int i = period; i < prices.size(); i++) {
-            ema = (prices.get(i) - ema) * multiplier + ema;
+            ema = (prices.get(i).doubleValue() - ema) * multiplier + ema;
             emaValues.add(ema);
         }
 
